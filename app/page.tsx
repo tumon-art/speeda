49f97491f95a40cb0495d319b893618110437249
tumon <tumonkhn@gmail.com>
2023-07-27 20:05:32 +0600

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import { ExampleTable } from "@/lib/drizzle";
 
-export default function Home() {
+export default async function Home() {
+  const data = await ExampleTable;
+  console.log(data);
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -15,7 +19,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -91,5 +95,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
