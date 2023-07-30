0eaf908cd498ba69a495c995a9eca400d0ddeb16
tumon <tumonkhn@gmail.com>
2023-07-30 07:46:08 +0600

    added pg and dotenv

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,19 @@
 import type { Config } from "drizzle-kit";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: ".env.development.local",
+});
+
+if (!process.env.POSTGRES_URL) {
+  throw new Error("POSTGRES_URL is missing");
+}
 
 export default {
   schema: "./drizzle/schema.ts",
   out: "./drizzle/migrations",
   driver: "pg",
   dbCredentials: {
-    connectionString: String(process.env.POSTGRES_URL),
+    connectionString: process.env.POSTGRES_URL,
   },
-  breakpoints: true,
 } satisfies Config;
